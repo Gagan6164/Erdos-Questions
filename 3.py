6bc568c76edca149575372c514d0fad84b7879ed
Gagan <gagansaini6164@gmail.com>
2018-09-24 22:03:00 +0530

    3 rd question

---
+++ 3.py
@@ -0,0 +1,15 @@
+import math
+for i in range(100000,1,-1):
+    y = 1
+    c = 0
+    b = []
+    while(c!=4):
+        if (math.sqrt(i*y**2+1)%1==0):
+            c=4
+            x = math.sqrt(i*y**2+1)
+            print(i,y,x)
+            b.append([x,i])
+        else:
+            y = y + 1
+    
+print(b)
