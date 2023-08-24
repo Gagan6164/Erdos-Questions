f988d46685cce54c8c389dae72ccb03143b38f95
Gagan6164 <32809202+Gagan6164@users.noreply.github.com>
2023-08-24 23:51:18 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,13 +1,8 @@
 a = 65
-for i in range(1,66):
-    c = i
-    b = a-c
-    a1 = b-c
-    b1 = a + c
-    b2 = a1 + c
-    b3 = a1 - c
-    c1 = b - a1
-    c2 = a1 + b
-    c3 = a + b
-    print(a,b,c)
-    print("  for a-->  ",a,a1,"  for b--> ",b,b2,b1,b3,"  for c-->  ",c,c1,c2,c3)
+# The key to solving the logical hats puzzles is as follows:
+# Each person knows that the number on his hat is either the sum or the difference of the other two. He also knows his number is positive.
+# Therefore with no other knowledge, A can only know the number on his hat if B and C are equal. Therefore A's "no" response rules out the numbers 2k,k,k.
+# Similarly B's "no" rules out k,2k,k and 2k,3k,k.
+# C's "no" rules out k,k,2k and 2k,k,3k and k,2k,3k and 2k,3k,5k.
+# A would say "no" unless the situation were 3k,2k,k or 4k,3k,k or 3k,k,2k or 4k,k,3k or 5k,2k,3k or 8k,3k,5k.
+# Therefore since A says yes, it must be one of those situations. The only one where A can have 65 is if B has 26 and C has 39.
